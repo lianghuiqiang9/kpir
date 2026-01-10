20318a958ab9101f977c91a8c525aaa8dcdb9f81
lianghuiqiang9 <lianghuiqiang92@gmail.com>
2026-01-10 05:33:32 +0000

    add kvs experiments

--- kvs/kvs_benchmark.sh
+++ kvs/kvs_benchmark.sh
@@ -2,11 +2,51 @@
 # bff-kvs
 go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=25 -bitsPerVal=32 -kvsID=bffkvs
 
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=24 -bitsPerVal=32 -kvsID=bffkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=23 -bitsPerVal=32 -kvsID=bffkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=22 -bitsPerVal=32 -kvsID=bffkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=21 -bitsPerVal=32 -kvsID=bffkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=20 -bitsPerVal=32 -kvsID=bffkvs
+
 # bbhashkvs
 go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=25 -bitsPerVal=32 -kvsID=bbhashkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=24 -bitsPerVal=32 -kvsID=bbhashkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=23 -bitsPerVal=32 -kvsID=bbhashkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=22 -bitsPerVal=32 -kvsID=bbhashkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=21 -bitsPerVal=32 -kvsID=bbhashkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=20 -bitsPerVal=32 -kvsID=bbhashkvs
 
 # pthashkvs
 go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=25 -bitsPerVal=32 -kvsID=pthashkvs
 
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=24 -bitsPerVal=32 -kvsID=pthashkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=23 -bitsPerVal=32 -kvsID=pthashkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=22 -bitsPerVal=32 -kvsID=pthashkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=21 -bitsPerVal=32 -kvsID=pthashkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=20 -bitsPerVal=32 -kvsID=pthashkvs
+
 # consensusrecsplitkvs
 go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=25 -bitsPerVal=32 -kvsID=consensusrecsplitkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=24 -bitsPerVal=32 -kvsID=consensusrecsplitkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=23 -bitsPerVal=32 -kvsID=consensusrecsplitkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=22 -bitsPerVal=32 -kvsID=consensusrecsplitkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=21 -bitsPerVal=32 -kvsID=consensusrecsplitkvs
+
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=20 -bitsPerVal=32 -kvsID=consensusrecsplitkvs
