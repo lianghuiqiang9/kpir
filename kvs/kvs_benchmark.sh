3b101fe378b4536ded6f439a00aad8fb739be658
lianghuiqiang9 <lianghuiqiang92@gmail.com>
2026-01-10 04:56:12 +0000

    add kvs experiments

--- kvs/kvs_benchmark.sh
+++ kvs/kvs_benchmark.sh
@@ -1,7 +1,12 @@
 
-
+# bff-kvs
 go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=25 -bitsPerVal=32 -kvsID=bffkvs
 
+# bbhashkvs
+go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=25 -bitsPerVal=32 -kvsID=bbhashkvs
+
+# pthashkvs
 go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=25 -bitsPerVal=32 -kvsID=pthashkvs
 
+# consensusrecsplitkvs
 go test -bench=BenchmarkKVStore -benchmem -run=none -v -args -logN=25 -bitsPerVal=32 -kvsID=consensusrecsplitkvs
